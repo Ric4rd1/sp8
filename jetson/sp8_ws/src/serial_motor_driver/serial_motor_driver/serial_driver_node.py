9f00b9fbd253c23477b6850ace1704e7e61e0a0d
Ricard Ubuntu <ric4rd11@gmail.com>
2026-04-19 20:24:03 -0600

    Implement reading wheel ang vel

--- jetson/sp8_ws/src/serial_motor_driver/serial_motor_driver/serial_driver_node.py
+++ jetson/sp8_ws/src/serial_motor_driver/serial_motor_driver/serial_driver_node.py
@@ -1,6 +1,8 @@
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
+from std_msgs.msg import Float32
+from rclpy import qos
 import serial
 
 
@@ -15,6 +17,9 @@ class SerialMotorDriver(Node):
 
         port = self.get_parameter('port').value
         baudrate = self.get_parameter('baudrate').value
+
+        self.msg_l = Float32()
+        self.msg_r = Float32()
 
         # Serial connection
         try:
@@ -32,6 +37,62 @@ class SerialMotorDriver(Node):
             10
         )
 
+        # Publishers
+        self.l_wheel_pub = self.create_publisher(
+            Float32,
+            '/wl',
+            qos_profile=qos.qos_profile_sensor_data
+        )
+
+        self.r_wheel_pub = self.create_publisher(
+            Float32,
+            '/wr',
+            qos_profile=qos.qos_profile_sensor_data
+        )
+
+
+        # Timer (reading serial wl and wr)
+        timer_period = 0.045
+        self.timer = self.create_timer(timer_period, self.timer_cb)
+    
+    def timer_cb(self):
+        if self.ser is None:
+            return
+
+        try:
+            if self.ser.in_waiting > 0:
+                line = self.ser.readline().decode('utf-8').strip()
+                #self.get_logger().info(f"Received: {line}")
+
+                # Expected:
+                # ang_vel_l: 1.2345, ang_vel_r: 1.5678
+
+                parts = line.split(',')
+
+                wl = None
+                wr = None
+
+                for part in parts:
+                    key, value = part.split(':')
+                    key = key.strip()
+                    value = value.strip()
+
+                    if key == 'ang_vel_l':
+                        wl = float(value)
+                    elif key == 'ang_vel_r':
+                        wr = float(value)
+
+                if wl is not None and wr is not None:
+                    
+                    self.msg_l.data = wl   # left wheel
+                    self.msg_r.data = wr   # right wheel
+
+                    self.l_wheel_pub.publish(self.msg_l)
+                    self.r_wheel_pub.publish(self.msg_r)
+
+        except Exception as e:
+            self.get_logger().warn(f"Read/parse error: {e}")
+
     def cmd_callback(self, msg: Twist):
         if self.ser is None:
             return
@@ -39,7 +100,7 @@ class SerialMotorDriver(Node):
         linear = msg.linear.x
         angular = msg.angular.z
 
-        # Example format → customize for your motor controller
+        # command format 
         command = f"linear: {linear}, angular: {angular}\n"
         self.get_logger().info(f'sending {command}')
 
